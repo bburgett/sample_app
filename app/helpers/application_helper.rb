35d1474a203ed6716acbb583700f4cb3794d4be1
Gregory L Nelson <sandmang102@gmail.com>
2010-09-17 06:14:00 -0700

    finished filling in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   # Return a title on a per-page basis.
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?

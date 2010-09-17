d8e5a13646e933c87def495b74d8b94b3fd2fddc
Gregory L Nelson <sandmang102@gmail.com>
2010-09-17 09:01:20 -0700

    began creation of User model

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,9 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  
+ 
+  map.resources :users
+
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
